db6556544f58866ea26c83c40320e7f3c576ec04
Troy Will <troydwill@gmail.com>
2011-05-27 19:56:05 -0700

    lib/perl-net-scan.pm: introduced "cell_hash" subroutine

--- usr/local/lib/perl-net-scan.pm
+++ usr/local/lib/perl-net-scan.pm
@@ -1,7 +1,8 @@
-sub scan_hash {
+sub scan_to_hash {
     my ( $interface, $scan_command ) = @_;
+    print "[$interface, $scan_command]\n";
     my ( %cell, %HoC, $mac, $security_type );
-    open (my $WIRELESS_SCAN, "$scan_command |") or die "Unable to open $scan_command: $!";
+    open (my $WIRELESS_SCAN, "$scan_command |") or die "Tried, but unable to open $scan_command: $!";
     while ( <$WIRELESS_SCAN> ) {
 	if ( m/^\s+Cell\s+\d+\s+-*\s*Address:\s*(([0-9a-fA-F]{2}[:-]{1}){5}([0-9a-fA-F]{2}))/ ) {
 	    $mac = $1;
@@ -15,11 +16,29 @@ sub scan_hash {
 	    $HoC{$mac}{Encryption} = $1;
 	    # security type
 	} elsif ( m/IEEE 802.11i\/WPA2 Version 1/ ) {
-	    $security_type .= "[WPA2]";
+	    $security_type .= "WPA2";
 	    $HoC{$mac}{security} = $security_type;
 	} elsif ( m/WPA Version 1/ ) {
-	    $security_type .= "[WPA]";
+	    $security_type .= "WPA";
 	    $HoC{$mac}{security} = $security_type;
+	}
+    }
+    return \%HoC;
+}
+
+sub cell_hash {
+    my ( $interface, $scan_command ) = @_;
+    my $HoC_ref = &scan_to_hash( $interface, $scan_command );
+    my %HoC = %{$HoC_ref};
+    foreach my $mac ( keys %HoC ) {
+	if ( $HoC{$mac}{Encryption} eq 'on' ) {
+	    print "DEBUG $HoC{$mac}{ESSID} =>$HoC{$mac}{security}<=\n";
+	    if ( ($HoC{$mac}{security} eq 'WPAWPA2') || ($HoC{$mac}{security} eq 'WPA2WPA')) {
+		$HoC{$mac}{security} = 'WPA+WPA2';
+	    }
+	    $HoC{$mac}{security} = 'WEP' if ( $HoC{$mac}{security} eq '' );
+	} else {
+	    print "NO security";
 	}
     }
     return \%HoC;
